c152641b1deb32826ac6d005931fc47cb864194b
Nicolás_Cuadra <nikocuadram@gmail.com>
2025-05-23 14:26:42 -0400

    Inicio del frontend

---
+++ frontend/src/app/about/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-20 gap-16">
+      <p>
+        Esta es la página de Acerca de Nosotros.
+      </p>
+    </div>
+  );
+}
